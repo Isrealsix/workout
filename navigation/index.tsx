16acb8d3b7c693ce469c35a8f83199bc21176c92
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-06-10 06:54:51 +0700

    configured navigation

---
+++ navigation/index.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import HomeScreen from "../screens/HomeScreen";
+import PlannerScreen from "../screens/Planner";
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Planner" component={PlannerScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+}

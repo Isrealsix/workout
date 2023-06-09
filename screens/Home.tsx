f2f5f6364048729864d90a71c67f9f918e3c8ef6
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-06-10 06:38:01 +0700

    added initial screens

---
+++ screens/Home.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>I am home o</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ screens/Planner.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Planner = () => {
+  return (
+    <View>
+      <Text>I am Planner o</Text>
+    </View>
+  )
+}
+
+export default Planner

040ca04892f9474ca806542616273da1b985e74e
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-01 01:16:08 +0700

    added buttons

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>I am home o</Text>
+      <Button 
+        title='Go to planner'
+        onPress={() => alert('Go to planner')}
+      />
     </View>
   )
 }
--- screens/Planner.tsx
+++ screens/Planner.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 const PlannerScreen = () => {
   return (
     <View>
       <Text>I am Planner o</Text>
+      <Button 
+        title='Go Home'
+        onPress={() => alert('Go to Home screen')}
+      />
     </View>
   )
 }

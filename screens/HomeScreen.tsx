8f08c7823323b7a4762b18bbd201aebfa16332d9
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-02 11:42:53 +0700

    imported data and added styles

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,16 +1,21 @@
-import { View, Text, Button } from "react-native";
-import { useNavigation } from "@react-navigation/core";
-import { ScreenProps } from "../types";
+import { View, Text, StyleSheet } from "react-native";
+import data from "../data.json";
+// import { useNavigation } from "@react-navigation/core";
+// import { ScreenProps } from "../types";
 
 const HomeScreen = () => {
-  const navigation = useNavigation<ScreenProps>();
+  // const navigation = useNavigation<ScreenProps>();
   return (
-    <>
-      <View>
-        <Text>I am home o</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text>I am home o</Text>
+      <Text>{JSON.stringify(data)}</Text>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 20
+  }
+})
 export default HomeScreen;

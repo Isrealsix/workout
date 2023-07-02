9b47b9ff92fd539006c646d18157c9916faa2b4c
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-02 11:52:31 +0700

    refactored renderItem to helper

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,21 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import data from "../data.json";
 // import { useNavigation } from "@react-navigation/core";
 // import { ScreenProps } from "../types";
 
 const HomeScreen = () => {
   // const navigation = useNavigation<ScreenProps>();
+  const renderItem = ({ item }: { item: any }) => (
+    <View>
+      <Text>{item.name}</Text>
+    </View>
+  );
   return (
     <View style={styles.container}>
-      <Text>I am home o</Text>
-      <Text>{JSON.stringify(data)}</Text>
+      {/* <Text>I am home o</Text> */}
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.slug}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20
-  }
-})
+    padding: 20,
+  },
+});
 export default HomeScreen;

a3c53c28ac9a31769cfa386670bf972925561f45
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-02 23:49:55 +0700

    list style flex and header on home

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import data from "../data.json";
 import { Workout } from "../types";
 import WorkoutItem from "../components/WorkoutItem";
@@ -9,6 +9,7 @@ const HomeScreen = () => {
   // const navigation = useNavigation<ScreenProps>();
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>New Workout</Text>
       <FlatList
         data={data as Array<Workout[number]>}
         renderItem={WorkoutItem}
@@ -21,6 +22,12 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+    flex: 1
   },
+  header: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold'
+  }
 });
 export default HomeScreen;

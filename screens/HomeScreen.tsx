cda82b67e7a5cdd047e937d3d0e908c542e3096f
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-02 11:34:39 +0700

    added data

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,25 +1,13 @@
 import { View, Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import { ScreenProps } from "../types";
-import { useEffect } from "react";
 
 const HomeScreen = () => {
   const navigation = useNavigation<ScreenProps>();
-  useEffect(() => {
-    console.log('mounted');
-
-    return () => {
-      console.log('unmounting');
-    }
-  })
   return (
     <>
       <View>
         <Text>I am home o</Text>
-        <Button
-          title="Go to planner"
-          onPress={() => navigation.navigate('PlannerScreen')}
-        />
       </View>
     </>
   );

4e9091b87045f5555e37b8bdece71c8558021e5b
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-16 23:39:20 +0700

    used useIsFocused hook

--- hooks/useWorkouts.ts
+++ hooks/useWorkouts.ts
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import { getWorkouts } from "../storage/workout";
 import { Workout } from "../types";
+import { useIsFocused } from "@react-navigation/native";
 
 export const useWorkouts = () => {
   const [workouts, setWorkouts] = useState<Workout>([]);
-  
+  const isFocused = useIsFocused()
+
   useEffect(() => {
     async function getData () {
       const _workouts = await getWorkouts();
       setWorkouts(_workouts);
     }
 
-    getData();
-  }, []);
+    if (isFocused) getData();
+  }, [isFocused]);
 
   return workouts;
 }

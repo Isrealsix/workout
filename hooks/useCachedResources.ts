d6e0628f1fd4b61d163781e8405b25ab74e0e8a5
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-16 22:11:19 +0700

    implemented storage

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import * as Font from 'expo-font';
+import { getData, storeData } from '../storage';
+import data from '../data.json';
 
 const useCachedResources = () => {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
   useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
+        await storeData('workout-data', data)
         await Font.loadAsync({
           "montserrat": require('../assets/fonts/Montserrat-Regular.ttf'),
           "montserrat-bold": require('../assets/fonts/Montserrat-Bold.ttf')
@@ -13,6 +16,8 @@ const useCachedResources = () => {
       } catch (error) {
         console.warn(error);
       } finally {
+        const workouts = await getData('workout-data');
+        console.log(workouts, 'of meee');
         setIsLoadingComplete(true);
       }
     }
---
+++ storage/index.tsx
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export const storeData = async (key: string, value: any) => {
+  try {
+    const stringValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, stringValue);
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+}
+
+export const getData = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (!value) return;
+    return JSON.parse(value);
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+}

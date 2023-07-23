1df14216df0685713e862824beaac17d0baacc1e
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-23 11:30:19 +0700

    added cleanup fnc

--- hooks/useCountdown.ts
+++ hooks/useCountdown.ts
@@ -1,22 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const useCountdown = (idx: number, initialCount: number) => {
+  const intervalRef = useRef<NodeJS.Timer>();
   const [countDown, setCountDown] = useState(initialCount);
   useEffect(() => {
     if (idx == -1) return;
 
-    const interval = setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setCountDown((prevCount) => {
         console.log(prevCount);
         return prevCount - 1;
       });
     }, 100);
-    return () => clearInterval(interval);
+    return cleanup;
   }, [idx]);
 
   useEffect(() => {
     setCountDown(initialCount);
   }, [initialCount]);
+
+  useEffect(() => {
+    if (countDown === 0) cleanup();
+  }, [countDown])
+
+  const cleanup = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = undefined
+    }
+  }
   return countDown;
 };
 

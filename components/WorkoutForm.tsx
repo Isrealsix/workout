50f929816ea681321b7cdd1a7b760c0a8a763854
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-24 02:36:16 +0700

    2 way bind text input

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -1,19 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const WorkoutForm = () => {
+  const [form, setForm] = useState({
+    name: '',
+    duration: ''
+  })
+
+  const handleTextChange = (name: string) => (text: string) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: text
+    }))
+
+    console.log(form);
+  }
   return (
     <View style={styles.container}>
       <Text>Excersice form</Text>
+      <View>
+        <TextInput
+          value={form.name}
+          style={styles.input}
+          onChangeText={handleTextChange('name')}
+        />
+        <TextInput
+          value={form.duration}
+          style={styles.input}
+          onChangeText={handleTextChange('duration')}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
     padding: 10
   }
-})
+});
 
 export default WorkoutForm;

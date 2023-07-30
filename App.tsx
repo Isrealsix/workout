7120938e772b786512f5bb7e08e72ae610cb5f9d
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-31 01:48:42 +0700

    safe area

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
 import Navigation from './navigation';
 import useCachedResources from './hooks/useCachedResources';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   const isLoaded = useCachedResources();
@@ -10,9 +11,9 @@ export default function App() {
   if (!isLoaded) return null;
   console.log(isLoaded);
   return (
-    <>
+    <SafeAreaProvider>
       <Navigation colorScheme={colorScheme} />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaProvider>
   );
 }

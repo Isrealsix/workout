c4e7304382b26d0b14c49d80ee953748effee67d
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-07-30 17:16:44 +0700

    styled text inputs

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -18,63 +18,67 @@ const WorkoutForm: React.FC<IProps> = ({ onSubmit }) => {
     <View style={styles.container}>
       <Text>Excersice form</Text>
       <View>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          name="name"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Name"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          name="duration"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Duration"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="reps"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Repetitions"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          name="type"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Type"
-            />
-          )}
-        />
+        <View style={styles.rowContainer}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Name"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="duration"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Duration"
+              />
+            )}
+          />
+        </View>
+        <View style={styles.rowContainer}>
+          <Controller
+            control={control}
+            name="reps"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Repetitions"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="type"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Type"
+              />
+            )}
+          />
+        </View>
         <PressableText
           text="Submit"
           onPress={handleSubmit((data) => {
@@ -94,12 +98,16 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   input: {
+    flex: 1,
     margin: 2,
     borderWidth: 1,
     height: 30,
     padding: 5,
     borderRadius: 5,
     borderColor: "rgba(0,0,0,0.4)",
+  },
+  rowContainer: {
+    flexDirection: "row",
   },
 });
 
